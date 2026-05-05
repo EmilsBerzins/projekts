d09d010b20df1b9783769ec668c7ae3c442b2bb9
EmilsBerzins <emilsberzins56@gmail.com>
2026-05-05 23:46:56 +0300

    Sakuma lapa un specifiska pilseta

--- app.py
+++ app.py
@@ -13,8 +13,24 @@ def get_db_connection():
 
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/pilsetas")
 def pilsetas():
     conn = get_db_connection()
     pilsetas = conn.execute("SELECT * FROM pilsetas").fetchall()
     conn.close()
-    return render_template("index.html", pilsetas=pilsetas)
+    return render_template("pilsetas_index.html", pilsetas=pilsetas)
+
+
+@app.route("/pilsetas/<int:id>")
+def pilseta(id):
+    conn = get_db_connection()
+    pilseta = conn.execute(
+        "SELECT pilsetas.id, nosaukums, iedzivotaji, platiba FROM pilsetas JOIN statistika ON pilsetas.statistika_id = statistika.id WHERE pilsetas.id = ?",
+        (id,),
+    ).fetchone()
+    conn.close()
+    return render_template("pilsetas_show.html", pilseta=pilseta)
