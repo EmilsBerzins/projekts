d0c8031df3573ab8416a406bc48e49ebd04182c8
EmilsBerzins <emilsberzins56@gmail.com>
2026-05-05 23:34:10 +0300

    Pilsetas

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from pathlib import Path
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    db = Path(__file__).parent / "db.sqlite"
+    conn = sqlite3.connect(db)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def pilsetas():
+    conn = get_db_connection()
+    pilsetas = conn.execute("SELECT * FROM pilsetas").fetchall()
+    conn.close()
+    return render_template("index.html", pilsetas=pilsetas)
